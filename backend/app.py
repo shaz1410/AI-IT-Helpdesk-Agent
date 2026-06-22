6e6bcc6525ad4b6b38055394c5235a6acbddadca
Shaz1410 <mthembusharon059@gmail.com>
2026-06-22 21:53:54 +0200

    updated the code by adding POST

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# -----------------------
+# Data Model (Ticket)
+# -----------------------
+class Ticket(BaseModel):
+    title:str = " internet connection"
+    description: str = " My internet connection is not working properly."
+    priority: str = "high"
+
+# -----------------------
+# In-memory database
+# -----------------------
+tickets = []
+
+# -----------------------
+# Root route
+# -----------------------
 @app.get("/")
 def home():
     return {"message": "AI Helpdesk API is running"}
+
+# -----------------------
+# Create ticket
+# -----------------------
+@app.post("/tickets")
+def create_ticket(ticket: Ticket):
+    tickets.append(ticket)
+    return {
+        "message": "Ticket created successfully",
+        "ticket": ticket
+    }
+
+# -----------------------
+# Get all tickets
+# -----------------------
+@app.get("/tickets")
+def get_tickets():
+    return tickets
